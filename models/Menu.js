cd27401ce4460767ca770b58ff8e7be86a200ef1
Thrugreen Admin <admin@thrugreen.net>
2020-12-27 10:01:41 +0100

    updates to menu data and model

--- models/Menu.js
+++ models/Menu.js
@@ -17,6 +17,10 @@ const MenuSchema = new mongoose.Schema({
 							type: Number,
 							required: [true, "Price for Menu Item is Required"],
 						},
+						itemMinimum: {
+							type: Number,
+							required: [true, "Item Minimum is Required"],
+						},
 						description: String,
 						modifiers: [
 							{
@@ -28,6 +32,7 @@ const MenuSchema = new mongoose.Schema({
 									{
 										name: String,
 										default: Boolean,
+										price: Number,
 									},
 								],
 							},
